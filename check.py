49b721706cf0f2b841bb6a52c54f0525ff5f6a24
Krrish Sehgal <133865424+krrish-sehgal@users.noreply.github.com>
2024-12-20 00:38:33 +0530

    hel

--- check.py
+++ check.py
@@ -1,10 +1,10 @@
 def filter_items(items: list, max_value: int = 100):
-filtered = [i for i in items if i < max_value]
-return filtered
+    filtered = [i for i in items if i < max_value]
+    return filtered
+
 
 from django.db import models
 
 class Account(models.Model):
-login_name = models.CharField(max_length=150)
-contact_email = models.EmailField(unique=True)
-
+    login_name = models.CharField(max_length=150)
+    contact_email = models.EmailField(unique=True)
